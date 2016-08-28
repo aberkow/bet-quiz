d845708cce567026598ccba09a1d8210309e3520
aberkow <berkowitz.clarinet@gmail.com>
2016-08-27 21:09:41 -0400

    switching branches to work on stepper

--- build/bundle.js
+++ build/bundle.js
@@ -70,7 +70,7 @@
 	
 	var _store2 = _interopRequireDefault(_store);
 	
-	var _Quiz = __webpack_require__(357);
+	var _Quiz = __webpack_require__(356);
 	
 	var _Quiz2 = _interopRequireDefault(_Quiz);
 	
@@ -30171,87 +30171,6 @@
 
 /***/ },
 /* 356 */
-/***/ function(module, exports) {
-
-	//
-	// const quiz = [
-	//   {
-	//     category: 'seating',
-	//     statement: 'Hold',
-	//     sanctuary: 'Individual seating - each person in a comfortable seat',
-	//     kessler: 'White cushioned folding chairs',
-	//     chapel: 'Cushioned bench chairs',
-	//   },
-	//   {
-	//     category: 'music',
-	//     statement: 'Hold',
-	//     sanctuary: 'Choir and organ',
-	//     kessler: 'Quiet band',
-	//     chapel: 'No music'
-	//   },
-	//   {
-	//     category: 'honors',
-	//     statement: 'Hold',
-	//     sanctuary: 'Pre-assigned to honor active members of our congregation',
-	//     kessler: 'Offered to groups = people will stand at their seats - abridged readings',
-	//     chapel: 'Given to those in attendance by ushers',
-	//   },
-	//   {
-	//     category: 'haftarah',
-	//     statement: 'Hold',
-	//     sanctuary: 'first half',
-	//     kessler: 'Last 10 verses',
-	//     chapel: 'Full'
-	//   },
-	//   {
-	//     category: 'shofar',
-	//     statement: 'Hold',
-	//     sanctuary: 'Al Reiner',
-	//     kessler: 'Noah Yontef Mathog',
-	//     chapel: 'Lance Reiser'
-	//   },
-	//   {
-	//     category: 'length',
-	//     statement: 'Hold',
-	//     sanctuary: '4+ hours',
-	//     kessler: '2+ hours',
-	//     chapel: '3+ hours'
-	//   },
-	//   {
-	//     category: 'liturgy',
-	//     statement: 'Hold',
-	//     sanctuary: 'Full service including silent prayer and choral singing',
-	//     kessler: 'Abridged "highlights" service, few silent moments, no repetition',
-	//     chapel: 'Full service with repetition'
-	//   },
-	//   {
-	//     category: 'experience',
-	//     statement: 'Hold',
-	//     sanctuary: 'Love the crowds - want to see and be seen',
-	//     kessler: 'Want to feel comfortable with my family',
-	//     chapel: 'Crave intimacy so I can focus on prayer'
-	//   },
-	//   {
-	//     category: 'clergy',
-	//     statement: 'Hold',
-	//     sanctuary: 'My High Holidays are not complete if I haven\'t heard Rabbi Rosen and Cantor Ness',
-	//     kessler: 'Rabbi Garber and Rabbi Sowalsky make a nice team and I appreciate the familiarity I feel in their service',
-	//     chapel: 'I\'d like to explore a lay-led service with Jason Kay and Susan Gold'
-	//   },
-	//   {
-	//     category: 'words',
-	//     statement: 'Hold',
-	//     sanctuary: 'Tradition, majesty, formality',
-	//     kessler: 'Family friendly, musical, soulful',
-	//     chapel: 'Intimate, intellectual, participatory'
-	//   }
-	// ];
-	//
-	// export default quiz;
-	"use strict";
-
-/***/ },
-/* 357 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -30264,7 +30183,7 @@
 	
 	var _reactRedux = __webpack_require__(172);
 	
-	var _quiz = __webpack_require__(356);
+	var _quiz = __webpack_require__(357);
 	
 	var _quiz2 = _interopRequireDefault(_quiz);
 	
@@ -30398,6 +30317,87 @@
 	var Container = (0, _reactRedux.connect)(mapStateToProps)(Quiz);
 	
 	module.exports = Container;
+
+/***/ },
+/* 357 */
+/***/ function(module, exports) {
+
+	//
+	// const quiz = [
+	//   {
+	//     category: 'seating',
+	//     statement: 'Hold',
+	//     sanctuary: 'Individual seating - each person in a comfortable seat',
+	//     kessler: 'White cushioned folding chairs',
+	//     chapel: 'Cushioned bench chairs',
+	//   },
+	//   {
+	//     category: 'music',
+	//     statement: 'Hold',
+	//     sanctuary: 'Choir and organ',
+	//     kessler: 'Quiet band',
+	//     chapel: 'No music'
+	//   },
+	//   {
+	//     category: 'honors',
+	//     statement: 'Hold',
+	//     sanctuary: 'Pre-assigned to honor active members of our congregation',
+	//     kessler: 'Offered to groups = people will stand at their seats - abridged readings',
+	//     chapel: 'Given to those in attendance by ushers',
+	//   },
+	//   {
+	//     category: 'haftarah',
+	//     statement: 'Hold',
+	//     sanctuary: 'first half',
+	//     kessler: 'Last 10 verses',
+	//     chapel: 'Full'
+	//   },
+	//   {
+	//     category: 'shofar',
+	//     statement: 'Hold',
+	//     sanctuary: 'Al Reiner',
+	//     kessler: 'Noah Yontef Mathog',
+	//     chapel: 'Lance Reiser'
+	//   },
+	//   {
+	//     category: 'length',
+	//     statement: 'Hold',
+	//     sanctuary: '4+ hours',
+	//     kessler: '2+ hours',
+	//     chapel: '3+ hours'
+	//   },
+	//   {
+	//     category: 'liturgy',
+	//     statement: 'Hold',
+	//     sanctuary: 'Full service including silent prayer and choral singing',
+	//     kessler: 'Abridged "highlights" service, few silent moments, no repetition',
+	//     chapel: 'Full service with repetition'
+	//   },
+	//   {
+	//     category: 'experience',
+	//     statement: 'Hold',
+	//     sanctuary: 'Love the crowds - want to see and be seen',
+	//     kessler: 'Want to feel comfortable with my family',
+	//     chapel: 'Crave intimacy so I can focus on prayer'
+	//   },
+	//   {
+	//     category: 'clergy',
+	//     statement: 'Hold',
+	//     sanctuary: 'My High Holidays are not complete if I haven\'t heard Rabbi Rosen and Cantor Ness',
+	//     kessler: 'Rabbi Garber and Rabbi Sowalsky make a nice team and I appreciate the familiarity I feel in their service',
+	//     chapel: 'I\'d like to explore a lay-led service with Jason Kay and Susan Gold'
+	//   },
+	//   {
+	//     category: 'words',
+	//     statement: 'Hold',
+	//     sanctuary: 'Tradition, majesty, formality',
+	//     kessler: 'Family friendly, musical, soulful',
+	//     chapel: 'Intimate, intellectual, participatory'
+	//   }
+	// ];
+	//
+	// export default quiz;
+	"use strict";
 
 /***/ },
 /* 358 */
